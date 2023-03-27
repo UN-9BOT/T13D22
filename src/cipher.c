9a266d96a8143d70bacc1551359c4ed4ea4bef54
Yajaira Hercules <un.9bot@gmail.com>
2023-03-27 20:31:07 +0300

    add ex3 but with bugs

--- src/cipher.c
+++ src/cipher.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
 
 void choice(int n, int *flag, char *path);
 void quest1(char *path);
 void quest2(char *path);
+void quest3();
+void shift(char *data, int n, char *nData, int sh);
+
 
 int main(void) {
     int n, trash;
@@ -32,6 +37,9 @@ void choice(int n, int *flag, char *path) {
             break;
         case -1:
             *flag = 0;
+            break;
+        case 3:
+            quest3();
             break;
         default:
             printf("n/a\n");
@@ -75,3 +83,61 @@ void quest2(char *path) {
         }
     }
 }
+
+void quest3() {
+    DIR *dir;
+    struct dirent *entry;
+    char pathDir[] = "ai_modules";
+
+
+    dir = opendir(pathDir);
+    if (!dir) {
+        printf("n/a");
+    } else {
+        while ((entry = readdir(dir)) != NULL) {
+            if ((entry->d_type == DT_REG) && (strstr(entry->d_name, ".h"))) {
+                char fifle[1200];
+                sprintf(fifle, "%s/%s", pathDir, entry->d_name);
+                FILE *fp = fopen(fifle, "w");
+                fclose(fp);
+            }
+            if ((entry->d_type == DT_REG) && (strstr(entry->d_name, ".txt"))) {
+                char fifle[1200];
+                sprintf(fifle, "%s/%s", pathDir, entry->d_name);
+                char *data = malloc(sizeof(char) * 3000);
+                char *nData = malloc(sizeof(char) * 3000);
+                int i = 0;
+                FILE *fp = fopen(fifle, "r");
+                while ((data[i++] = fgetc(fp)) != EOF) {};
+                shift(data, i, nData, -2);
+                fclose(fp);
+                FILE *fpw = fopen(fifle, "w");
+                while (i != 0) {
+                    fputc(data[i--], fpw);
+                }
+                fclose(fpw);
+            }
+        }
+        closedir(dir);
+    }
+}
+
+void shift(char *data, int n, char *nData, int sh) {
+    if (sh > 0) {
+        for (int i = 0, k = n - sh; i < n; i++, k++) {
+            if (k >= n) {
+                k = 0;
+            }
+            *(nData + k) = *(data + i);
+        }
+    } else {
+        sh = -sh;
+        for (int i = 0, k = sh; i < n; i++, k++) {
+            if (k >= n) {
+                k -= n;
+            }
+            *(nData + k) = *(data + i);
+        }
+    }
+}
+
